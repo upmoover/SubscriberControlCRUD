aec808e7b0d523613d541839e2703a916378c641
upmoover <upmoover@gmail.com>
2021-06-13 16:19:38 +0300

    Init commit

---
+++ src/main/java/com/upmoover/SubscriberControlCRUD/SubscriberControlCrudApplication.java
@@ -0,0 +1,13 @@
+package com.upmoover.SubscriberControlCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriberControlCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriberControlCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upmoover/SubscriberControlCRUD/SubscriberControlCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upmoover.SubscriberControlCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriberControlCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
